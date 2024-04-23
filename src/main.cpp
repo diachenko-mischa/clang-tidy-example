5600e72ac7e0f4d05fb6e0e55073b1c126e1c928
Mykhailo Diachenko <ext-mykhailo.z.diachenko@here.com>
2024-04-23 10:30:13 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <array>
+#include <iostream>
+
+struct Foobar {
+ private:
+  int m_helloField;
+};
+
+int main(int argc, char** argv) {
+  constexpr auto ARRAY_SIZE = 100;
+  std::array<int, ARRAY_SIZE> array;
+  std::cout << "Hello world" << std::endl;
+  return 0;
+}

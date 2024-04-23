484c027a2f51addd96ec991fb954d746b1ba1e7c
Mykhailo Diachenko <ext-mykhailo.z.diachenko@here.com>
2024-04-23 17:59:24 +0300

    adds sqlite

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <array>
 #include <iostream>
 
-struct f__Foobar {
+struct Foobar {
  private:
   int m_helloField;
 };
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
   constexpr auto ARRAY_SIZE = 100;
   std::array<int, ARRAY_SIZE> array;
   std::cout << "Hello world" << std::endl;
